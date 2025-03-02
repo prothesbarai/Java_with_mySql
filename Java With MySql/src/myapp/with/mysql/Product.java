78f3bedd82c3e63485c42017b8a1e57453334ecc
prothesbarai <aspprothesbarai1619@gmail.com>
2025-03-02 23:59:51 +0600

    v1.0.0.1

---
+++ Java With MySql/src/myapp/with/mysql/Product.java
@@ -0,0 +1,11 @@
+package myapp.with.mysql;
+public class Product {
+    public static void main(String[] args) {
+       
+        
+        System.out.println("kkk");
+        
+        
+        
+    }
+}
